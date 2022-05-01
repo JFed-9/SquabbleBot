8e87b0b250a1ebd8f4d1c61b03fe673bb6312884
Jackson Feddock <jackson@xlr8dev.com>
2022-05-01 15:22:58 -0700

    Updated Wordlist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pyautogui
-import ImageGrab
+import time
 
 row1 = 420
 col1 = 820
@@ -9,26 +9,35 @@ Purple = 0x9B5DF7
 
 
 def MakeGuess(word, row, currlist):
-    pyautogui.write(word, interval=0.05)
+    pyautogui.write(word)
     win = True
     for letterIndex in range(5):
         columnPixel = col1 + letterIndex * 75
         # Get Pixel information for this pixel: row1, columnPixel
-        color = 0xFFFFFF
+        color = Purple
         if color == Green:
-            currlist = filter(lambda option: option[letterIndex] == word[letterIndex], currlist)
+            currlist = list(filter(lambda option: option[letterIndex] == word[letterIndex], currlist))
         if color == Yellow:
             win = False
-            currlist = filter(lambda option: option[letterIndex] != word[letterIndex] and word[letterIndex] in option,
-                              currlist)
+            currlist = list(filter(lambda option: option[letterIndex] != word[letterIndex] and word[letterIndex] in option, currlist))
         if color == Purple:
             win = False
-            currlist = filter(lambda option: word[letterIndex] not in option, currlist)
-    return win
+            currlist = list(filter(lambda option: word[letterIndex] not in option, currlist))
+    return win, currlist
 
 
 if __name__ == '__main__':
-    with open('Wordlist.txt', 'r') as wordlist:
-        list = wordlist.readlines()
+    time.sleep(3)
+    wordlist = []
+    with open("Wordlist.txt", "r") as file:
+        for line in file:
+            wordlist.append(line[0:5])
 
-    checkWin = MakeGuess('paint', 1, list)
+    print(len(wordlist))
+
+    checkWin, wordlist = MakeGuess('paint', 1, wordlist)
+    checkWin, wordlist = MakeGuess('model', 1, wordlist)
+    # checkWin, wordlist = MakeGuess('crush', 1, wordlist)
+
+    print(len(wordlist))
+    print(wordlist)

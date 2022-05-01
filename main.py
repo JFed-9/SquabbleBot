893092f5c3fb16704ca2491856d1e7ec19617d7f
Jackson Feddock <jackson@xlr8dev.com>
2022-05-01 14:21:23 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyautogui
+
+row1 = 420
+col1 = 820
+Green = 0x2ED83C
+Yellow = 0xD6BE00
+Purple = 0x9B5DF7
+
+def MakeGuess(word, row, currlist):
+    moddedlist = []
+    pyautogui.write(word, interval=0.05)
+
+
+if __name__ == '__main__':
+    pyautogui.alert("test")

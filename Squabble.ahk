0efc41ebb318f183cb9a141cf59bee719de4925e
Jackson Feddock <jackson@xlr8dev.com>
2022-05-02 07:24:27 -0700

    Working v0.5

---
+++ Squabble.ahk
@@ -0,0 +1,27 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+#SingleInstance Force
+
+^p::
+Send, !{Tab}
+Sleep 100
+Send, +{F10}
+Sleep, 100
+Send, !{Tab}
+return
+
+^l::
+Send, !{Tab}
+Sleep 100
+Send, ^{F2}
+Sleep, 100
+Send, !{Tab}
+return
+
+^t::
+Send, {Enter}
+return
+
+^Esc:: ExitApp
--- main.py
+++ main.py
@@ -1,53 +1,18 @@
 import pyautogui
 import time
-import ImageGrab
+from PIL import ImageGrab
 
-row1 = 420
-col1 = 820
-Green = 0x2ED83C
-Yellow = 0xD6BE00
-Purple = 0x9B5DF7
+row1 = 470
+col1 = 425
+Green = (46, 216, 60)
+Yellow = (214, 190, 0)
+Purple = (155, 93, 247)
 
-
-def MakeGuess(word, row, currlist):
-    pyautogui.write(word)
-    win = True
-    for letterIndex in range(5):
-        columnPixel = col1 + letterIndex * 75
-        # Get Pixel information for this pixel: row1, columnPixel
-        color = Purple
-        if color == Green:
-            currlist = list(filter(lambda option: option[letterIndex] == word[letterIndex], currlist))
-        if color == Yellow:
-            win = False
-            currlist = list(filter(lambda option: option[letterIndex] != word[letterIndex] and word[letterIndex] in option, currlist))
-        if color == Purple:
-            win = False
-            currlist = list(filter(lambda option: word[letterIndex] not in option, currlist))
-    return win, currlist
-
-
-if __name__ == '__main__':
-    time.sleep(3)
-    wordlist = []
-    with open("Wordlist.txt", "r") as file:
-        for line in file:
-            wordlist.append(line[0:5])
-
-    print(len(wordlist))
-
-    checkWin, wordlist = MakeGuess('paint', 1, wordlist)
-    checkWin, wordlist = MakeGuess('model', 1, wordlist)
-    # checkWin, wordlist = MakeGuess('crush', 1, wordlist)
-
-    print(len(wordlist))
-    print(wordlist)
-
-
-def checkLetterResult(row=1, col=1):
+def checkLetterResult(col=1, row=1):
     image = ImageGrab.grab()
 
     color = image.getpixel((row1 + (row-1)*75, (col1 + (col-1)*75)))
+    # pyautogui.moveTo((row1 + (row-1)*75, (col1 + (col-1)*75)))
 
     if color == Green:
         return 2
@@ -56,4 +21,53 @@ def checkLetterResult(row=1, col=1):
     elif color == Purple:
         return 0
     else:
-        return "NEW" + str(color)
+        print("NEW" + str(color))
+        time.sleep(5)
+        return -1
+
+def MakeGuess(word, row, currlist):
+    pyautogui.write(word)
+    time.sleep(0.1)
+    pyautogui.hotkey('ctrl', 't')
+    time.sleep(0.3)
+
+    win = True
+    for letterIndex in range(5):
+        color = checkLetterResult(row, letterIndex + 1)
+        if color == 2:
+            currlist = list(filter(lambda option: option[letterIndex] == word[letterIndex], currlist))
+        if color == 1:
+            win = False
+            currlist = list(filter(lambda option: option[letterIndex] != word[letterIndex] and word[letterIndex] in option, currlist))
+        if color == 0:
+            win = False
+            currlist = list(filter(lambda option: word[letterIndex] not in option, currlist))
+    return win, currlist
+
+
+if __name__ == '__main__':
+    time.sleep(1)
+    wordlistbackup = []
+    wordlist = []
+    with open("Wordlist.txt", "r") as file:
+        for line in file:
+            wordlistbackup.append(line[0:5])
+
+    while True:
+        wordlist = wordlistbackup
+        checkWin, wordlist = MakeGuess('paint', 1, wordlist)
+        checkWin, wordlist = MakeGuess('model', 2, wordlist)
+        checkWin, wordlist = MakeGuess('crush', 3, wordlist)
+
+        currRow = 4
+
+        while checkWin is not True and len(wordlist) > 0:
+            checkWin, wordlist = MakeGuess(wordlist[0], currRow, wordlist)
+            currRow += 1
+
+        if not checkWin:
+            break
+        time.sleep(1.5)
+
+    print(len(wordlist))
+    print(wordlist)
